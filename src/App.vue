a252d1317df413ee14fe60be977688d1e7a25e4a
feifan0620 <feifan0620@qq.com>
2024-09-09 11:56:39 +0800

    ``` 添加路由配置和基础样式 ```

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-
+.box{
+  height: 300px;
+  width: 300px;
+  background-color: pink;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/views/layout'
+import Login from '@/views/login'
+import MyOrder from '@/views/myorder'
+import Pay from '@/views/pay'
+import ProDetail from '@/views/prodetail'
+import Search from '@/views/search'
+import SearchList from '@/views/search/list'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: '/', component: Layout },
+    { path: '/login', component: Login },
+    { path: '/myOrder', component: MyOrder },
+    { path: '/pay', component: Pay },
+    { path: '/prodetail/:id', component: ProDetail },
+    { path: '/search', component: Search },
+    { path: '/searchlist', component: SearchList }
+  ]
 })
 
 export default router
