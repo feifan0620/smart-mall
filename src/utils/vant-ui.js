39375c083bb3d9ff01c8008db4d0579bb5cfcb05
feifan0620 <feifan0620@qq.com>
2024-09-09 12:30:32 +0800

    集成VantUI组件库和Tabbar布局的实现

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
-import { Button } from 'vant'
+import { Button, Tabbar, TabbarItem } from 'vant'
 
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
 Vue.use(Button)
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>我是layout</div>
+  <div>
+    <van-tabbar active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="list-switching">分类</van-tabbar-item>
+      <van-tabbar-item icon="cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
