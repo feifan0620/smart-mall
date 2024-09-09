218f98360804a2378e369edf4c8c67d682de2886
feifan0620 <feifan0620@qq.com>
2024-09-09 12:59:12 +0800

    实现布局页面的子路由配置与Tabbar导航的关联

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,26 @@ import ProDetail from '@/views/prodetail'
 import Search from '@/views/search'
 import SearchList from '@/views/search/list'
 
+import Home from '@/views/layout/home'
+import Category from '@/views/layout/category'
+import Cart from '@/views/layout/cart'
+import User from '@/views/layout/user'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: Layout },
+    {
+      path: '/',
+      component: Layout,
+      redirect: '/home',
+      children: [
+        { path: '/home', component: Home },
+        { path: '/category', component: Category },
+        { path: '/cart', component: Cart },
+        { path: '/user', component: User }
+      ]
+    },
     { path: '/login', component: Login },
     { path: '/myOrder', component: MyOrder },
     { path: '/pay', component: Pay },
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <van-tabbar active-color="#ee0a24" inactive-color="#000">
-      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="list-switching">分类</van-tabbar-item>
-      <van-tabbar-item icon="cart-o">购物车</van-tabbar-item>
-      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    <router-view></router-view>
+    <van-tabbar route active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item to="/home" icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/category" icon="list-switching">分类</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="manager-o">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
